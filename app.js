8a6f3ebd70895d3202659bdff628b983a1610ab0
goodnice123 <futurewei13800@gmail.com>
2025-08-31 11:00:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { exec, execSync } = require('child_process');
 
 // 环境变量
 const UUID = process.env.UUID || '231f268c-d7a8-436f-a097-3e58040e42e7'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.future10000.eu.org';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'al2XWEwFTGTFUMjKwe';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
